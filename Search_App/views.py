513e49364cc7424786f0daaf3ed113f3d1812462
raihanalam <raihanhridoybd@gmail.com>
2022-02-11 06:14:59 +0600

    filtering improved

--- Search_App/views.py
+++ Search_App/views.py
@@ -1,13 +1,16 @@
+from lib2to3.pgen2.token import GREATER
+from time import time
 from django.shortcuts import render
 from .models import Search_History
 from django.contrib.auth.models import User
 import json
 from django.http import JsonResponse
 from django.core import serializers
+from datetime import datetime, timedelta
+from django.utils import timezone
 # Create your views here.
 
 def home(request):
-
      if request.method == 'POST':
           keyword = request.POST.get('keyword')
           user = request.user
@@ -24,8 +27,10 @@ def home(request):
      all_keywords = Search_History.objects.all()
      all_dist_keywords = Search_History.objects.values('keyword').distinct()
 
+     #Counting all single keyword
      keyword_count = []
      all_single_keys = []
+
      for dist_key in all_dist_keywords:
           count = 0
           for key in all_keywords:
@@ -43,27 +48,99 @@ def home(request):
 
 
 def filter(request):
+
+     #Any process that you want
+     #Getting all data from JS
      keyword = request.GET.get('keyword')
      users = request.GET.get('users')
-     key_list = keyword.split(',')
+     time_range = request.GET.get('times')
 
-     user_list = users.split(',')
+     try:
+          #Splitting them to list
+          key_list = keyword.split(',')
+          user_list = users.split(',')
+          time_list = time_range.split(',')
+
+          #Checking the maximum time range
+          max_time = 0
+          for i in time_list:
+               i = int(i)
+               if i > max_time:
+                    max_time = i
+          
+          #Decreasing the time based on time range
+          #now_datetime = datetime.now()
+          now_datetime = timezone.now()
+
+          if max_time == 1:
+               range_time = now_datetime - timedelta(hours=24)
+          elif max_time == 7:
+               range_time = now_datetime - timedelta(hours=24*7)
+          elif max_time == 30:
+               range_time = now_datetime - timedelta(hours=24*30)
+          
+
+          #print(datetime.date(range_time))
+
+          #start_date = datetime.date(2005, 1, 1)
+          #end_date = datetime.date(2005, 3, 31)
+          
+
+          start_date = datetime.date(range_time)
+          end_date = datetime.date(now_datetime)
+     except:
+          print("Something error!")
 
      myUser = User.objects.filter(username__in = user_list)
+
+     #print(start_date,end_date)
      #print(key_list)
+     if keyword == '' and users == '' and time_range == '': 
+          print('All fields are empty.')
+     elif keyword != '' and users == '' and time_range == '':
+          search_objects = Search_History.objects.filter(keyword__in = key_list,)
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
 
-     # Any process that you want
+     elif keyword !='' and users != '' and time_range == '':
 
-     search_objects = Search_History.objects.filter(user__in = myUser) and Search_History.objects.filter(keyword__in = key_list)
-     #search_ob = list(search_objects)
-     #search_ob = json.dumps(search_objects)
-     filter_result = serializers.serialize('json', search_objects)
-     search_new = json.loads(filter_result)
-    
+          search_objects = Search_History.objects.filter(user__in = myUser, keyword__in = key_list,)
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
 
+     elif users != '' and keyword == '' and time_range == '':
+          search_objects = Search_History.objects.filter(user__in = myUser)
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
      
+     elif time_range != '' and users != '' and keyword == '' :
+          search_objects = Search_History.objects.filter(user__in = myUser, time__range=(start_date, end_date))
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
+
+     elif time_range != '' and users == '' and keyword == '' :
+          search_objects = Search_History.objects.filter(time__range=(start_date, end_date))
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
+
+     elif time_range != '' and users == '' and keyword != '' :
+          search_objects = Search_History.objects.filter(keyword__in = key_list,time__range=(start_date, end_date))
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
+          
+     elif keyword  != '' and users != '' and time_range != '':
+          search_objects = Search_History.objects.filter(user__in = myUser, keyword__in = key_list, time__range=(start_date, end_date)) #and Search_History.objects.filter(time__range=(start_date, end_date)) # and Search_History.objects.filter(keyword__in = key_list, time__range=(start_date, end_date) )
+          #search_ob = list(search_objects)
+          #search_ob = json.dumps(search_objects)
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
+     else:
+          search_objects = Search_History.objects.filter(user__in = myUser, keyword__in = key_list)
+          filter_result = serializers.serialize('json', search_objects)
+          search_new = json.loads(filter_result)
+
     
-     print(search_new)
+     #print(search_new)
      data = {
           'all_search_data': search_new
      }
